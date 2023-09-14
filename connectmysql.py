72e9b9351954a5ba69aa54cf8df163899ba309b9
Samit Koyom <samitkoyom@gmail.com>
2023-09-14 10:56:08 +0700

    update connect mysql to cleaver cloud

--- connectmysql.py
+++ connectmysql.py
@@ -2,11 +2,19 @@ import pymysql
 
 # สร้างฟังก์ชันสำหรับการเชื่อมต่อฐานข้อมูล
 def connectdb():
+    # connection = pymysql.connect(
+    #     host='localhost',
+    #     user='root',
+    #     password='12345678',
+    #     db='pythondb',
+    #     port=3306,
+    #     cursorclass=pymysql.cursors.DictCursor
+    # )
     connection = pymysql.connect(
-        host='localhost',
-        user='root',
-        password='12345678',
-        db='pythondb',
+        host='bsxlwlo7mm54jimf3oid-mysql.services.clever-cloud.com',
+        user='utkrhrvd3nsx8fn6',
+        password='1OQu9GrOWtr5sw1DOO4l',
+        db='bsxlwlo7mm54jimf3oid',
         port=3306,
         cursorclass=pymysql.cursors.DictCursor
     )
